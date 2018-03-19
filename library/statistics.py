f8440539aac154f01bf7a4ff8cec84f9366c8b39
@thien <thien@users.noreply.github.com>
2018-03-19 19:50:49 +0000

    add support for generating charts on learning rate and move count

--- library/statistics.py
+++ library/statistics.py
@@ -1,5 +1,11 @@
 import json
 import os
+import numpy as np
+import seaborn as sns
+from matplotlib import cm
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import ast
 
 class Statistics:
   def __init__(self, date, defaultResultsPath=None):
@@ -8,11 +14,11 @@ class Statistics:
     if defaultResultsPath:
       self.path = defaultResultsPath
     self.statistics = {}
+    self.directory = os.path.join(self.path, self.date)
     leaderboards = []
   
   def loadStatisticsFile(self, filename="statistics.json"):
-    directory = os.path.join(self.path, date)
-    filepath = os.path.join(directory, filename)
+    filepath = os.path.join(self.directory, filename)
     print("Loading Statistics from file:")
     print("\t", filepath)
     f = open(filepath, 'r')
@@ -23,6 +29,9 @@ class Statistics:
   def parseLeaderboards(self):
     for gen in self.statistics:
       pass
+
+  def saveChartToFile(self, plt, title):
+    savefig('foo.png', bbox_inches='tight')
 
   """
   Gets tournament timing information
@@ -41,25 +50,96 @@ class Statistics:
       # sort the duration times
       times['gameDurations'] = sorted(times['gameDurations'])
       x.append(times)
+
+    # generate a graph for this
     return x
 
   """
-  Calculates the average number of moves per generation
+  Calculates the average number of moves per generation.
   """
   def averageNumMovesPerGeneration(self):
-    print("Calculating Means..")
-    x = []
+    print("Calculating Move Counts (Per Generation)")
+    means = []
+    medians = []
+    graphData = {
+      "x" : [],
+      "y" : []
+    }
+    counter = 0
     for i in range(len(self.statistics)):
       moveCounts = []
       for j in self.statistics[i]['games']:
-        moveCounts.append(len(j['Moves']))
-    mean = sum(x) / float(len(x))
-    print("Mean:", mean)
-    x.append(mean)
-    return x
+        graphData['x'].append(i)
+        graphData['y'].append(len(j['game']['Moves']))
+        moveCounts.append(len(j['game']['Moves']))
+        counter += 1
+      moveCounts = sorted(moveCounts)
+      mean = sum(moveCounts) / float(len(moveCounts))
+      # print(i, "Mean:", round(mean,2), "S:", moveCounts[0], "L:", moveCounts[-1])
+      means.append(mean)
+      medians.append(max(set(moveCounts), key=moveCounts.count))
+
+    mean_colour = "red"
+    median_colour = "yellow"
+    # add legend
+    mean_patch = mpatches.Patch(color=mean_colour, label='Mean')
+    median_patch = mpatches.Patch(color=median_colour, label='Median')
+    plt.legend(handles=[mean_patch,median_patch])
+
+    # plot 2d histogram
+    plt.hist2d(graphData['x'], graphData['y'], bins=50)
+    # plot line graph of means
+    plt.plot(means, '--', linewidth=2, color=mean_colour)
+    plt.plot(medians, '--', linewidth=2, color=median_colour)
+    # needs title
+    plt.ylabel('Number of Moves')
+    plt.xlabel('Generation')
+    plt.colorbar()
+
+    plt.show()
+
+    return means
+
+  def getLearningRate(self):
+    scores = []
+    cummulative = []
+    champRange = []
+    print("Getting learning Rate..")
+    for i in range(len(self.statistics)):
+      for j in self.statistics[i]['stats']:
+        if j[0] == "Recent Scores":
+          scores.append(float(ast.literal_eval(j[1])[-1]))
+        if j[0] == "Cummulative Score":
+          cummulative.append(float(j[1]))
+        if j[0] == "Prev. Champ Point Range":
+          champRange.append(ast.literal_eval(j[1]))
+
+    
+    # verify if we can compute the same cummulative score
+    # base = 0
+    # for i in range(len(cummulative)):
+    #   base += round(scores[i],2)
+    #   print(base, cummulative[i])
+    # convert champion range into floats
+    for i in range(len(champRange)):
+      li = champRange[i]
+      for j in range(len(li)):
+        li[j] = float(li[j])
+      champRange[i] = li
+    
+    # for i in champRange:
+    #   print(i)
+
+    # plot cummulative graph
+    plt.plot(cummulative, '--', linewidth=2, color='blue')
+    # needs title
+    plt.ylabel('Learning Rate')
+    plt.xlabel('Generation')
+    plt.show()
 
 if __name__ == '__main__':
   date = "2018-03-19 16:42:47"
   s = Statistics(date)
   s.loadStatisticsFile()
-  print(s.averageNumMovesPerGeneration)
+  # s.averageNumMovesPerGeneration()
+  s.getLearningRate()
