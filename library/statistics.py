b3231f366bdcc98d6f8c14238a3cb5781f481ecf
@thien <thien@users.noreply.github.com>
2018-03-20 01:17:34 +0000

    create charts of gold master stats

--- library/statistics.py
+++ library/statistics.py
@@ -18,6 +18,8 @@ class Statistics:
     self.directory = os.path.join(self.path, self.date)
     leaderboards = []
     self.saveChartsToImages = True
+    self.gmFilename = "gm_stats.json"
+    self.enableTitles = True
   
   def loadStatisticsFile(self, filename="statistics.json"):
     filepath = os.path.join(self.directory, filename)
@@ -99,7 +101,8 @@ class Statistics:
     # needs title
     plt.ylabel('Seconds')
     plt.xlabel('Generation')
-    plt.suptitle('Game Run Time Over Generations')
+    if self.enableTitles:
+      plt.suptitle('Game Run Time Over Generations')
     # plt.colorbar()
     # set axis
     x1,x2,y1,y2 = plt.axis() # get current axis
@@ -152,7 +155,8 @@ class Statistics:
     # needs title
     plt.ylabel('Number of Moves')
     plt.xlabel('Generation')
-    plt.suptitle('Move Count Distribution Over Generations')
+    if self.enableTitles:
+      plt.suptitle('Move Count Distribution Over Generations')
     plt.colorbar()
 
     # plt.show()
@@ -195,7 +199,8 @@ class Statistics:
     plt.axhline(0, color='grey')
     plt.plot(cummulative, '--', linewidth=2, color='blue')
     # needs title
-    plt.suptitle('Cummulative Learning Rate Over Generations')
+    if self.enableTitles:
+      plt.suptitle('Cummulative Learning Rate Over Generations')
     plt.ylabel('Learning Rate (Cummulative)')
     plt.xlabel('Generation')
     # plt.show()
@@ -207,7 +212,8 @@ class Statistics:
     plt.axhline(0, color='grey')
     plt.plot(scores, '--', linewidth=2, color='blue')
     # needs title
-    plt.suptitle('Champion Scores Over Generations')
+    if self.enableTitles:
+      plt.suptitle('Champion Scores Over Generations')
     plt.ylabel('Scores')
     plt.xlabel('Generation')
     # plt.show()
@@ -216,10 +222,104 @@ class Statistics:
     plt.close()
 
 
+  """
+  Load GM file
+  """
+  def loadGMFile(self):
+    filepath = os.path.join(self.directory, self.gmFilename)
+    print("Loading Statistics from file:")
+    print("\t", filepath)
+    f = open(filepath, 'r')
+    self.gm_stats = json.load(f)
+    f.close()
+    print("Loaded GM Stats File!")
+
+  """
+  Parse the GM performance
+  """
+  def analyseGM(self):
+    opp_names = []
+    w,l,d = [], [], []
+    aw_w, aw_l, aw_d = [],[],[]
+    ab_w, ab_l, ab_d = [],[],[]
+
+    # load the gm_stats file.load
+    for i in self.gm_stats.keys():
+      intel = self.gm_stats[i]
+      # print(intel)
+      opp_name = intel['opp'].upper().replace("ERATION", "")
+      opp_names.append(opp_name)
+      w.append(intel['wins'])
+      l.append(intel['losses'])
+      d.append(intel['draws'])
+      aw_w.append(intel['as_white']['wins'])
+      aw_l.append(intel['as_white']['losses'])
+      aw_d.append(intel['as_white']['draws'])
+      ab_w.append(intel['as_black']['wins'])
+      ab_l.append(intel['as_black']['losses'])
+      ab_d.append(intel['as_black']['draws'])
+    
+    # for i in range(len(opp_names)):
+    #   print(ab_w[i], aw_w[i], ab_d[i], aw_d[i], ab_l[i], aw_l[i])
+
+    # create overal wld chart
+    t = "Overall Game Statistics Against All Opponments"
+    data = [aw_w, aw_d, aw_l, ab_w, ab_d, ab_l]
+    self.createWDLChart(opp_names,data,t)
+    # plt.show()
+    if self.saveChartsToImages:
+      self.saveChartToFile("gm_net_stats",plt)
+    plt.close()
+    
+    return False
+
+  def createWDLChart(self, opp_names,data,title=None):
+    ww,dw,lw,wb,db,lb = data
+    #  normalise values to 100%
+    totals = [i+j+k+l+m+n for i,j,k,l,m,n in zip(ww,dw,lw,wb,db,lb)]
+    ww = [i / j * 100 for i,j in zip(ww, totals)]
+    dw = [i / j * 100 for i,j in zip(dw, totals)]
+    lw = [i / j * 100 for i,j in zip(lw, totals)]
+    wb = [i / j * 100 for i,j in zip(wb, totals)]
+    db = [i / j * 100 for i,j in zip(db, totals)]
+    lb = [i / j * 100 for i,j in zip(lb, totals)]
+
+    r = [x for x in range(len(opp_names))]
+
+    # plot
+    names = opp_names
+    # Create green Bars
+    plt.bar(r, wb, color='#27375a', edgecolor='white',  label="Win (Black)")
+    plt.bar(r, ww, bottom=wb, color='#377bae', edgecolor='white',  label="Win (White)")
+    # Create orange Bars
+    plt.bar(r, db, bottom=[i+j for i,j in zip(wb,ww)], color='#c340a9', edgecolor='white',  label="Draw (Black)")
+    plt.bar(r, dw, bottom=[i+j+k for i,j,k in zip(wb,ww,db)], color='#9310ab', edgecolor='white',  label="Draw (White)")
+    # Create blue Bars
+    plt.bar(r, lb, bottom=[i+j+k+l for i,j,k,l in zip(wb,ww,db,dw)], color='#d3d460', edgecolor='white',  label="Loss (Black)")
+    plt.bar(r, lw, bottom=[i+j+k+l+m for i,j,k,l,m in zip(wb,ww,db,dw,lb)], color='#eeac0e', edgecolor='white',  label="Loss (White)")
+    
+    # Custom x axis
+    plt.xticks(r, names)
+    plt.xlabel("Opponments")
+    plt.ylabel("Percentage")
+
+    plt.legend(loc='upper left', bbox_to_anchor=(1,1), ncol=1)
+    if self.enableTitles:
+      if title:
+        plt.suptitle(title)
+
+  def saveCharts(self):
+    self.averageNumMovesPerGeneration()
+    self.getLearningRate()
+    self.timeStatsPerGeneration()
+
 if __name__ == '__main__':
   date = "2018-03-19 16:42:47"
   s = Statistics(date)
-  s.loadStatisticsFile()
-  s.averageNumMovesPerGeneration()
-  s.getLearningRate()
-  s.timeStatsPerGeneration()
+  # s.loadStatisticsFile()
+  # s.saveCharts()
+  s.loadGMFile()
+  s.analyseGM()
+  # s.averageNumMovesPerGeneration()
+  # s.getLearningRate()
+  # s.timeStatsPerGeneration()
