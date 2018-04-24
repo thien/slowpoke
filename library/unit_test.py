429972730a551e9017fbefeffd36b25dae5204e4
@thien <thien@users.noreply.github.com>
2018-04-24 22:47:58 +0100

    add comment on unit-tests specifying that this is only for inter-classes

--- library/unit_test.py
+++ library/unit_test.py
@@ -8,21 +8,10 @@ random.seed(1)
 
 import unittest
 
-# class TestStringMethods(unittest.TestCase):
-
-#   def test_upper(self):
-#     self.assertEqual('foo'.upper(), 'FOO')
-
-#   def test_isupper(self):
-#     self.assertTrue('FOO'.isupper())
-#     self.assertFalse('Foo'.isupper())
-
-#   def test_split(self):
-#     s = 'hello world'
-#     self.assertEqual(s.split(), ['hello', 'world'])
-#     # check that s.split fails when the separator is not a string
-#     with self.assertRaises(TypeError):
-#       s.split(2)
+"""
+These unit tests cover situations that are hard to cover since they normally take a while to produce.
+Normally, the classes have tests within their files.
+"""
 
 class PopulationTestCase(unittest.TestCase):
   def setUp(self):
