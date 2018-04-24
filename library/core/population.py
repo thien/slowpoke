82829f02d0158cddda09e6b30e19d7c7cf5ad2da
@thien <thien@users.noreply.github.com>
2018-04-24 22:36:45 +0100

    add support for safe mutations

--- library/core/population.py
+++ library/core/population.py
@@ -1,6 +1,7 @@
 import agents.slowpoke as sp
 import agents.agent as agent
 import core.mongo as mongo
+from agents.evaluator.neural import NeuralNetwork
 
 import math
 import random
@@ -8,13 +9,18 @@ import numpy as np
 import json
 import operator
 import os
+import datetime
+import multiprocessing
 
 Black, White, empty = 0, 1, -1
 
 WinPt, DrawPt, LosePt = 2, 0, -1
 
 class Population:
-  def __init__(self, numberOfPlayers, plyDepth):
+  def __init__(self, numberOfPlayers, plyDepth, isDebug=False):
+    # used for testing purposes.
+    self.isDebug = isDebug
+    
     self.generation = 0
     self.count = numberOfPlayers
     self.plyDepth = plyDepth  # plydepth
@@ -34,13 +40,21 @@ class Population:
     # check save directory exists prior to saving
     if not os.path.isdir(self.folderDirectory):
       os.makedirs(self.folderDirectory)
+    # if safe mutations are enabled, we use it.
+    self.safeMutations = False
+    # create reference neural network
+    self.nn = NeuralNetwork(self.players[0].bot.layers)
+    # debug flag
+    self.debug = False
+    # flag to handle crossover method
+    self.crossoverMethod = 1
 
   """
   Generates an individual player. This is only called in the
   generatePlayers() function!
   """
   def generatePlayer(self):
-    bot = sp.Slowpoke(self.plyDepth)
+    bot = sp.Slowpoke(self.plyDepth,debug=self.isDebug)
     human = agent.Agent(bot)
     # generate ID
     human.setID(self.playerCounter)
@@ -67,7 +81,8 @@ class Population:
   how good they are (in terms of points)
   """
   def printCurrentPopulationByPoints(self):
-    print("Current Population:",self.currentPopulation)
+    if self.debug:
+      print("Current Population:",self.currentPopulation)
     points = list(map(lambda x: (x,self.players[x].points), self.currentPopulation))
     # sort list of tuples
     output = ""
@@ -93,45 +108,49 @@ class Population:
   Output: a new list of players.
   """
   def generateNextPopulation(self):
+    start = datetime.datetime.now()
     # increment generation count
     self.generation += 1
     # start with the top 5 players from the previous generation
-    offsprings = self.currentPopulation[:5]
-    # reset scores for these current offsprings
-    for i in offsprings:
-      self.players[i].points = 0
+    elites = self.currentPopulation[:5]
+    # reset scores for these elites
+    for i in elites: self.players[i].points = 0
+    # create list of offsprings
+    offsprings = []
     # create magic crossover from new parents
     for i in range(0,2):
       # # get ID's of parents
       parent_a_ID, parent_b_ID = self.currentPopulation[i], self.currentPopulation[i+1]
-      # # here we create 4 new children
+      # here we create 4 new children
       children = self.generatePlayers(4)
-      # generate random cutoff positions
-      index1 = random.randint(0, self.numberOfWeights)
-      index2 = random.randint(0, self.numberOfWeights)
-      # check the order of the indexes to make sure they make sense.
-      if index1 > index2: 
-        index1, index2 = index2, index1
+
       # crossover from parents
-      children[0], children[1] = self.crossOver(parent_a_ID, parent_b_ID, children[0], children[1], index1, index2)
+      children[0], children[1] = self.crossOver(parent_a_ID, parent_b_ID, children[0], children[1])
 
       # assign evolution blocks to child 0 and child 1.
       self.inheritOrigins(children[0], [parent_a_ID,parent_b_ID])
       self.inheritOrigins(children[1], [parent_b_ID,parent_a_ID])
+      # copy the caches of the parent to the child
+      self.inheritCache(children[0],parent_a_ID)
+      self.inheritCache(children[1],parent_b_ID)
+      
       self.addOrigins(children[0], [1,1,0])
       self.addOrigins(children[1], [1,1,0])
 
       # for the last 2 offsprings, they obtain the same weights as their parents.
-      self.setWeights(children[2],self.getWeights(i))
-      self.setWeights(children[3],self.getWeights(i+1))
+      self.setWeights(children[2],self.getWeights(parent_a_ID))
+      self.setWeights(children[3],self.getWeights(parent_b_ID))
       self.addOrigins(children[2], [0,1,0])
       self.addOrigins(children[3], [0,1,0])
-      self.inheritOrigins(children[2], [self.currentPopulation[i]])
-      self.inheritOrigins(children[3], [self.currentPopulation[i+1]])
-
+      self.inheritOrigins(children[2], [parent_a_ID])
+      self.inheritOrigins(children[3], [parent_b_ID])
+      # copy the caches of the parent to the child
+      self.inheritCache(children[2],parent_a_ID)
+      self.inheritCache(children[3],parent_b_ID)
+      
       # mutate all offsprings
-      for offspring in children:
-        self.mutate(offspring)
+      # for offspring in children:
+      #   self.mutate(offspring)
       # now we add children to the list of offsprings
       offsprings = offsprings + children
 
@@ -143,34 +162,73 @@ class Population:
     self.addOrigins(remainders[1], [0,1,0])
     self.inheritOrigins(remainders[0], [self.currentPopulation[3]])
     self.inheritOrigins(remainders[1], [self.currentPopulation[4]])
+    # copy the caches of the parent to the child
+    self.inheritCache(remainders[0], self.currentPopulation[3])
+    self.inheritCache(remainders[1], self.currentPopulation[4])
 
-    for offspring in remainders:
-      self.mutate(offspring)
     # add remainders to list of offsprings
     offsprings = offsprings + remainders
+    if self.debug:
+      print("offsprings:",offsprings)
+   
+    # mutate the offsprings. we should parallelise this.
+    cores = multiprocessing.cpu_count()
+    mutations = []
+    with multiprocessing.Pool(processes=cores) as pool:
+      mutations = pool.map(self.mutate, offsprings)
+
+    # now that we have the mutations, load them to each agent.
+    for mutation in mutations:
+      self.players[mutation[0]].bot.nn.loadCoefficents(mutation[1])
+
+    newPopulation = offsprings + elites
     # assign this set of offsprings as the new population.
-    self.currentPopulation = offsprings
-    self.count = len(offsprings)
-  
+    self.currentPopulation = newPopulation
+    self.count = len(self.currentPopulation)
+    end = datetime.datetime.now() - start
+    if self.debug:
+      print("DONE, that took", end)
+
   """
   Crossover mechanism for creating offspring children
   Input: two parents, two children, two indexes to swap from
   """
-  def crossOver(self, cpu1, cpu2, child1, child2, index1, index2):
+  def crossOver(self, cpu1, cpu2, child1, child2):
     """
     Basic Crossover Algorithm for the GA.
     """
+    if self.debug:
+      print("Implementing Crossover for IDs "+ str(child1) +"," +str(child2), end=".. ")
     mother = self.getWeights(cpu1)
     father = self.getWeights(cpu2)
-
-    # pythonic crossover
-    child1W = np.append(np.append(father[:index1], mother[index1:index2]), father[index2:])
-    child2W = np.append(np.append(mother[:index1], father[index1:index2]), mother[index2:])
     
-    # create new children with it
-    self.setWeights(child1, child1W)
-    self.setWeights(child2, child2W)
-
+    if self.crossoverMethod == 0:
+      for _ in range(10):
+        # generate a random index and swap genes
+        index = random.randint(0, self.numberOfWeights)
+        genome_m = mother[index]
+        genome_f = father[index]
+        mother[index] = genome_m
+        father[index] = genome_f
+      self.setWeights(child1, mother)
+      self.setWeights(child2, father)
+    
+    else:
+      # generate random cutoff positions
+      index1 = random.randint(0, self.numberOfWeights)
+      index2 = random.randint(0, self.numberOfWeights)
+      # check the order of the indexes to make sure they make sense.
+      if index1 > index2: 
+        index1, index2 = index2, index1
+      # pythonic crossover
+      child1W = np.append(np.append(father[:index1], mother[index1:index2]), father[index2:])
+      child2W = np.append(np.append(mother[:index1], father[index1:index2]), mother[index2:])
+      
+      # create new children with it
+      self.setWeights(child1, child1W)
+      self.setWeights(child2, child2W)
+    
+    print("Done.")
     # return the pair of children
     return (child1,child2)  
 
@@ -181,27 +239,96 @@ class Population:
     """
     Mutate the weights of the neural network.
     """
-    # # generate a random number
-    # chance = random.random()
-    # if chance >= (1-self.mutationRate):
-    #   # mutate the weights.
-    #   weights = self.getWeights(cpu)
-    #   length = weights.size
-    #   base1 = weights[random.randint(0,length-1)]
-    #   base2 = weights[random.randint(0,length-1)]
-    #   temp = base1
-    #   np.place(weights, weights==base1, temp)
-    #   np.place(weights, weights==base2, base1)
-    #   np.place(weights, weights==temp, base2)
-    #   self.setWeights(cpu, weights)
+    if self.debug:
+      print("Generating mutations for player " + str(cpu))
+
     weights = self.getWeights(cpu)
-    # random mutation multipliers
-    multipliers = np.random.random_sample([self.numberOfWeights])
-    multipliers = self.tau * multipliers
-    multipliers = np.exp(multipliers)
-    weights=weights*multipliers
-    weights=np.clip(weights, -1, 1)
-    self.setWeights(cpu, weights)
+    # weights = self.players[cpu].bot.nn.weights
+    # check whether their moves are cached.
+    moveBase = self.getMoveCache(cpu)
+
+    if self.safeMutations and len(moveBase) > 100:
+      weights = self.safeMutation(cpu)
+      # nuke the cache
+      self.players[cpu].bot.cache = {}
+    else:
+      # random mutation multipliers
+      multipliers = np.random.random_sample([self.numberOfWeights])
+      multipliers = self.tau * multipliers
+      multipliers = np.exp(multipliers)
+      weights=weights*multipliers
+      weights=np.clip(weights, -1, 1)
+      self.setWeights(cpu, weights)
+    if self.debug:
+      print("Finished mutations for player " + str(cpu))
+    return (cpu, weights)
+
+  """
+  Static function to create safe mutations
+  """
+  def safeMutation(self, cpu, static=False):
+    cache = self.getMoveCache(cpu)
+    curreneWeight1D = self.players[cpu].bot.nn.getAllCoefficents()
+
+    # get a subset of those cached moves.
+    subset = {}
+    subsetSize = int(len(cache.keys())/10)
+    if subsetSize < 1000:
+      subsetSize = len(cache.keys())
+    for _ in range(subsetSize):
+      rand = random.choice(list(cache.keys()))
+      subset[rand] = np.array(rand)
+
+    # now we find the safest mutation
+    bestWeight = curreneWeight1D
+    bestScore = 0
+
+    for su in range(100):
+      weights = None
+      if static:
+        # create a new mutation
+        multipliers = np.random.random_sample([self.numberOfWeights])
+        multipliers = self.tau * multipliers
+        multipliers = np.exp(multipliers)
+        weights = multipliers * curreneWeight1D
+        weights = np.clip(weights, -1, 1)
+        self.players[cpu].bot.nn.loadCoefficents(weights)
+      else:
+        for w in range(len(self.players[cpu].bot.nn.weights)):
+          weights = self.players[cpu].bot.nn.weights[w]
+          multipliers = np.random.random_sample(weights.shape)
+          multipliers = self.tau * multipliers
+          multipliers = np.exp(multipliers)
+          # print(weights.shape, multipliers.shape)
+          self.players[cpu].bot.nn.weights[w] = np.add(weights,multipliers)
+          self.players[cpu].bot.nn.weights[w] = np.clip(self.players[cpu].bot.nn.weights[w],-1,1)
+
+          biases = self.players[cpu].bot.nn.biases[w]
+          multipliers = np.random.random_sample(biases.shape)
+          multipliers = self.tau * multipliers
+          multipliers = np.exp(multipliers)
+          # print(weights.shape, multipliers.shape)
+          self.players[cpu].bot.nn.biases[w] = np.add(biases,multipliers)
+          self.players[cpu].bot.nn.biases[w] = np.clip(self.players[cpu].bot.nn.biases[w],-1,1)
+        weights = self.players[cpu].bot.nn.getAllCoefficents()
+
+      # calculate to see whether these weights are better
+      qa = 0
+      for i in subset:
+        # evaluate this cached move
+        eval_a = self.players[cpu].bot.nn.compute(subset[i])
+        # compare it to the current value
+        if eval_a/cache[i] >= 0.95:
+          qa += 1
+      if qa > bestScore:
+        bestWeight = weights
+        bestScore = qa
+        # print("New best:", qa, su)
+      percentile = round(bestScore*100/len(subset),2)
+      print(str(cpu)+" - Count:"+str(su)+" Best:"+str(bestScore)+"/"+str(len(subset))+ " - " +str(percentile)+"%\r",end="")
+    # print("\nDONE")
+    return bestWeight
+
 
   """
   Saves champions to a file.
@@ -301,6 +428,16 @@ class Population:
   def getWeights(self,botID):
     return self.players[botID].bot.nn.getAllCoefficents()
 
+  """
+  Helper function to retrieve cache if it exists,
+  otherwise return false.
+  """
+  def getMoveCache(self,botID):
+    if self.players[botID].bot.enableCache:
+      return self.players[botID].bot.cache
+    else:
+      return False
+
   # Done
   def addOrigins(self,botID, values):
     self.players[botID].origin = [values]
@@ -310,6 +447,8 @@ class Population:
     for i in parentIDs:
        self.players[botID].parents.append(i)
 
+  def inheritCache(self,botID,parentID):
+    self.players[botID].bot.cache = self.players[parentID].bot.cache
   # # Done
   # @staticmethod
   # def randomPlayerID(val):
@@ -356,3 +495,39 @@ class Population:
   #       chosen = players[i]
   #       break
   #   return chosen
+
+  @staticmethod
+  def generateRandomWeights(weights=None):
+    multipliers = np.random.random_sample([self.numberOfWeights])
+    multipliers = self.tau * multipliers
+    multipliers = np.exp(multipliers)
+    if np.any(weights):
+      weights=weights*multipliers
+      weights=np.clip(weights, -1, 1)
+      return weights
+    else:
+      return np.clip(multipliers,-1, 1)
+
+  @staticmethod
+  def generateFakeMoves():
+    print("Generating fake moves", end=".. ")
+    # we create a dictionary of fake moves. 
+    nn = NeuralNetwork(layer_list=[91,40,10,1])
+    fakeMoves = {}
+    for _ in range(10000):
+      # generate a random list of nn inputs
+      state = np.random.random_sample([91])
+      stateID = tuple(state)
+      evals = nn.compute(state)
+      fakeMoves[stateID] = evals
+      # evaluate it
+    # get a copy of the current nn coefs.
+    coefs = nn.getAllCoefficents()
+    # return this.
+    print("Done.")
+    return (fakeMoves, coefs)
+    
+
+if __name__ == '__main__':
+  # we can use this to test out the population program
+  pass
